be22a503c63c056ef00596e85d5fc127cc5ca4a0
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2023-10-11 16:36:14 -0400

    begin cbo cards; build

---
+++ data/enums.js
@@ -0,0 +1,9 @@
+const BOROUGHS = {
+    brooklyn: 'Brooklyn',
+    queens: 'Queens',
+    bronx: 'The Bronx',
+    manhattan: 'Manhattan',
+    staten: 'Staten Island'
+};
+
+export {BOROUGHS};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './index.less';
+import {cboData} from '../data/cbos';
+import CboTemplate from './templates/cboTemplate.handlebars';
 
 $(document).ready(() => {
-    initTranslation();
+    setTimeout(initTranslation, 100);
+
+    $('.filter-btn').on('click', ({currentTarget}) => {
+        const filterType = $(currentTarget).data('filter');
+        console.log(filterType);
+    });
+
+    $('#cbo-content').html(
+        cboData.map((data) => CboTemplate(data)).join('')
+    );
 });
 
 function initTranslation() {
